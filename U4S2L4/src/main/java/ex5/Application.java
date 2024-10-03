30b97bfde90a10ac5061a596aa7b7d03f2386620
oBeta97 <paolopellizzari97@gmail.com>
2024-10-03 16:21:27 +0200

    esercizio 5

--- U4S2L4/src/main/java/ex5/Application.java
+++ U4S2L4/src/main/java/ex5/Application.java
@@ -1,8 +1,31 @@
 package ex5;
 
+import org.epicode.entities.Product;
+import org.epicode.utils.Utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        List<Product> randomProducts = Utils.getRandomProducts(20);
+
+        Map<String, Double> billCategory =
+            randomProducts
+                .stream()
+                .collect(
+                    Collectors.groupingBy(
+                        Product::getCategory,
+                        Collectors.summingDouble(Product::getPrice)
+                    )
+                )
+        ;
+
+        billCategory.forEach((cat, imports) -> System.out.println("Categoria: " + cat + " - totale: " + imports));
+
     }
 }
