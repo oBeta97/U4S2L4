9d4db373ba33bb04025f727d5b3cbcc2d6da4166
oBeta97 <paolopellizzari97@gmail.com>
2024-10-03 16:06:52 +0200

    esercizio 4

--- U4S2L4/src/main/java/ex4/Application.java
+++ U4S2L4/src/main/java/ex4/Application.java
@@ -1,8 +1,33 @@
 package ex4;
 
+import org.epicode.entities.Order;
+import org.epicode.entities.Product;
+import org.epicode.utils.Utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        List<Order> randomOrders = Utils.getRandomListOfOrder(20);
+
+        System.out.println(randomOrders);
+
+        Double avg = randomOrders.stream()
+            .collect(
+                    Collectors.averagingDouble(order ->
+                        order
+                            .getProducts().stream()
+                                .mapToDouble(Product::getPrice)
+                                .sum()
+                    )
+            )
+        ;
+
+        System.out.println(avg);
     }
 }
