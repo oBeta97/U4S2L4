bc2272005a958d6ab76b946fc18bc7b757a0f05f
oBeta97 <paolopellizzari97@gmail.com>
2024-10-03 15:34:50 +0200

    esercizio 3

--- U4S2L4/src/main/java/ex3/Application.java
+++ U4S2L4/src/main/java/ex3/Application.java
@@ -1,8 +1,32 @@
 package ex3;
 
-public class Application {
+import org.epicode.entities.Product;
+import org.epicode.utils.Utils;
 
+import java.util.Comparator;
+import java.util.List;
+
+public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        List<Product> randomProducts = Utils.getRandomProducts(5);
+
+        System.out.println(randomProducts);
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("*************************************** ORDINAMENTO ***************************************");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        List<Product> expensiveProduct = randomProducts.stream()
+                .sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+                .limit(3)
+                .toList();
+
+        System.out.println("I 3 prodotti più costosi sono:");
+        for (Product p : expensiveProduct) System.out.println(p);
+
     }
 }
